8fde587e6b1212cc4c137dc186c12c5095ac42e4
BounSweFerhatSal <ferhat.sal@boun.edu.tr>
2020-05-03 18:12:34 +0300

    static folder added to project

---
+++ NA_Project/NA_WebApp/static/NA_WebApp/js/home.js
@@ -0,0 +1,38 @@
+(function($){
+
+	"use strict";
+	
+	var home = {
+		
+		load : function() {
+
+			window.dynamicNumbersBound = false;
+			home.bindDynamicNumbers();
+
+		},
+		bindDynamicNumbers : function () {
+			$('.dynamic-number').each(function() {				
+				var startNumber = $(this).text();
+				var endNumber = $(this).data('dnumber');
+				var dynamicNumberControl = $(this);
+				
+				$({numberValue: startNumber}).animate({numberValue: endNumber}, {
+					duration: 4000,
+					easing: 'swing',
+					step: function() { 
+						$(dynamicNumberControl).text(Math.ceil(this.numberValue)); 
+					}
+				});
+			});	
+		},	
+		hasClass : function (element, cls) {
+			return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+		}		
+
+	};
+	
+	$(document).ready(function () {
+		home.load();
+	});
+
+})(jQuery);
---
+++ NA_Project/NA_WebApp/static/NA_WebApp/js/scripts.js
@@ -0,0 +1,85 @@
+(function($){
+
+	"use strict";
+	  
+	$(document).ready(function () {
+		socialchef.init();
+	});
+	
+	var socialchef = {
+	
+		init: function () {
+			//CUSTOM FORM ELEMENTS
+			$('select, input[type=radio],input[type=checkbox],input[type=file]').uniform();
+			
+			//MOBILE MENU
+			$('.main-nav').slicknav({
+				prependTo:'.head .wrap',
+				allowParentLinks : true,
+				label:''
+			});
+			
+			//SCROLL TO TOP BUTTON
+			$('.scroll-to-top').click(function () {
+				$('body,html').animate({
+					scrollTop: 0
+				}, 800);
+				return false;
+			});
+			
+			//MY PROFILE TABS 
+			$('.tab-content').hide().first().show();
+			$('.tabs li:first').addClass("active");
+
+			$('.tabs a').on('click', function (e) {
+				e.preventDefault();
+				$(this).closest('li').addClass("active").siblings().removeClass("active");
+				$($(this).attr('href')).show().siblings('.tab-content').hide();
+			});
+
+			var hash = $.trim( window.location.hash );
+			if (hash) $('.tab-nav a[href$="'+hash+'"]').trigger('click');
+			
+			//ALERTS
+			$('.close').on('click', function (e) {
+				e.preventDefault();
+				$(this).closest('.alert').hide(400);
+			});
+			
+			//CONTACT FORM 
+			$('#contactform').submit(function(){
+			
+				var action = $(this).attr('action');
+				
+				$("#message").show(400,function() {
+				$('#message').hide();
+				
+				$('#submit')
+					.after('<img src="images/contact-ajax-loader.gif" class="loader" />')
+					.attr('disabled','disabled');
+				
+				$.post(action, { 
+					name: $('#name').val(),
+					email: $('#email').val(),
+					phone: $('#phone').val(),
+					comments: $('#comments').val()
+				},
+					function(data){
+						document.getElementById('message').innerHTML = data;
+						$('#message').slideDown('slow');
+						$('#contactform img.loader').fadeOut('slow',function(){$(this).remove()});
+						$('#submit').removeAttr('disabled'); 
+						//if(data.match('success') != null) $('#contactform').slideUp(3000);
+						
+					}
+				);
+				});
+				return false; 
+			});
+			
+			//PRELOADER
+			$('.preloader').fadeOut();
+		},
+	}
+
+})(jQuery);
